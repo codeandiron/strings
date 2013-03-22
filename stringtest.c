5dfbbf556a6ed6a07ffe3d241332dcff9a78212c
Matt Holtom <mattholtom@gmail.com>
2013-03-22 12:27:31 -0400

    Fixed a bug in stringtest.c where destination was uninitialized causing segfault. I can't remember, but this might have been the original intent of the application, as it was originally a question about buffer overflows... hmm... Fixed it anyways just in case someone actually clones this.

--- stringtest.c
+++ stringtest.c
@@ -1,10 +1,11 @@
 #include "stdio.h"
+#include "stdlib.h"
 #include "strcpy.h"
 
 int main(int argc, char **argv) {
 
 	const char * STRINGTOCOPY = "Hello World";
-	char * destination;
+	char * destination = (char *) malloc(100);
 	
 	printf("\n Destination is: ");
 	puts(destination);
